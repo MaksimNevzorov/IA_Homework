751a9c1a53adfe04b33861ffc3fc7cae30c146c0
MaksimNevzorov <nevzorov.work@yandex.by>
2022-10-04 16:37:54 +0300

    "ANKETA" task completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./main.scss";
+import { ANKETA } from "./components/ANKETA/ANKETA";
+
+const root = document.querySelector("#root");
 
 const add123 = (a, b) => a + b;
-
 const res = add123(222, 3);
 console.log("Helloasda");
+
+root.insertAdjacentHTML("beforeend", ANKETA());
