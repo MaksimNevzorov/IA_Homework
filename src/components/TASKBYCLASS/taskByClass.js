a516e06dae4bbc40b7ddc2af4dd03deb5237afba
MaksimNevzorov <nevzorov.work@yandex.by>
2022-10-14 01:02:04 +0300

    completed task 'MyString'

--- src/components/TASKBYCLASS/taskByClass.js
+++ src/components/TASKBYCLASS/taskByClass.js
@@ -18,4 +18,34 @@ export const taskByClass = () => {
   console.log(worker.rate);
   console.log(worker.days);
   console.log(worker.getSalary());
+
+  class MyString {
+    constructor(str) {
+      this.str = str;
+    }
+    reverse(str) {
+      let newStr = str.split("").reverse().join("");
+      return newStr;
+    }
+    ucFirst(str) {
+      let newStr = str[0].toUpperCase() + str.slice(1);
+      return newStr;
+    }
+    ucWords(str) {
+      let arrStr = str.split(" ");
+      let newStr = [];
+      for (let i = 0; i < arrStr.length; i++) {
+        let word = arrStr[i];
+        let upWord = word[0].toUpperCase() + word.slice(1);
+        newStr.push(upWord);
+      }
+      return newStr.join(" ");
+    }
+  }
+
+  const myString = new MyString();
+
+  console.log(myString.reverse("abcde")); //выведет 'edcba'
+  console.log(myString.ucFirst("abcde")); //выведет 'Abcde'
+  console.log(myString.ucWords("abcde abcde abcde")); //выведет 'Abcde Abcde Abcde'
 };
